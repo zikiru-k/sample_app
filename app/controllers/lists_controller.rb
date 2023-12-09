a41734b20169a9c5af7c89b13232b23f9c4a74b3
zikiru-k <149685941+zikiru-k@users.noreply.github.com>
2023-12-09 06:42:44 +0000

    Initial Commit

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,46 @@
+class ListsController < ApplicationController
+  def new
+    # Viewへ渡すためのインスタンス変数に空のModelオブジェクトを生成する。
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list.id)
+    else
+      render :new
+    end
+  end
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+    # 今回はレコードを1件だけ取得するので、インスタンス変数名は単数形の「@list」
+    @list = List.find(params[:id])
+  end
+
+  def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
+  end
+
+  def destroy
+    list = List.find(params[:id]) # データ（レコード）を1件取得
+    list.destroy # データ（レコード）を削除
+    redirect_to '/lists'  # 投稿一覧画面へリダイレクト
+  end
+
+  private
+  # ストロングパラメーター
+  def list_params
+    params.require(:list).permit(:title, :body, :image)
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  get 'lists/new'
+
+  get 'top' => 'homes#top'
+  post 'lists' => 'lists#create'
+  get 'lists' => 'lists#index'
+  get 'lists/:id' => 'lists#show', as: 'list'
+  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
+  patch 'lists/:id' => 'lists#update', as: 'update_list'
+  delete 'lists/:id' => 'lists#destroy', as: 'destroy_list'
+end
